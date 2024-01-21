fc855382034cab962edc9264ec583c187853a791
Petar <vlajic.p27@gmail.com>
2024-01-21 21:46:09 +0100

    Fixes 6.0

--- src/pages/Stations.tsx
+++ src/pages/Stations.tsx
@@ -136,7 +136,7 @@ const Stations = () => {
               onClick={openModal}
               className="bg-blue-500 text-white px-4 py-2 rounded-md border"
             >
-              Add Location
+              Dodaj novo stajaliste
             </button>
             <ModalForm isOpen={isModalOpen} onClose={closeModal} />
 
